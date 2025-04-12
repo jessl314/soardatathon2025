254e677e22179c5a7af85ed9d7d551a3ba212361
arenago126 <arenagaleanaoregonn@gmail.com>
2025-04-12 13:44:16 -0700

    pages created

---
+++ my-app/src/DataPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DataPage() {
+    return (
+        <div style={{ backgroundColor: '#fff', height: '100%', textAlign: 'center', paddingTop: '50px' }}>
+            <h1>Data Page</h1>
+            <p>Welcome to the Data Page.</p>
+        </div>
+    );
+}
+
+export default DataPage
---
+++ my-app/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Home Page</h1>
+      {/* Add your home page content here */}
+    </div>
+  );
+}
+
+export default HomePage; 
